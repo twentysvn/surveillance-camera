234e30a387a6bd3c57d34019499336d217860f61
twentysvn <qodim.is30@gmail.com>
2020-03-14 23:04:59 +0700

    coba algoritma HOG

--- simulasi.py
+++ simulasi.py
@@ -2,13 +2,15 @@ import cv2
 
 object_classifier = cv2.CascadeClassifier("models/fullbody_recognition_model.xml")
 
+
 def mulai_simulasi(classifier):
-    path = "test/3.jpg"
+    path = "test/5.jpg"
     file = cv2.imread(path)
     gray = cv2.cvtColor(file, cv2.COLOR_BGR2GRAY)
+    ret, frame = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
 
     objects = classifier.detectMultiScale(
-        gray,
+        frame,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
@@ -17,14 +19,21 @@ def mulai_simulasi(classifier):
 
     if len(objects) > 0:
         print("terdeteksi")
+    else:
+        print("!!!! tidak terdeteksi !!!!")
+
     for (x, y, w, h) in objects:
         cv2.rectangle(file, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     for (x, y, w, h) in objects:
         cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+    for (x, y, w, h) in objects:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
     cv2.imwrite('hasil_tes.jpg', file)
     cv2.imwrite('hasil_tes_gray.jpg', gray)
+    cv2.imwrite('hasil_tes_gray_treshold.jpg', frame)
 
 
 mulai_simulasi(object_classifier)
---
+++ simulasi_HOG.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cv2.startWindowThread()
+
+# open webcam video stream
+
+# Capture frame-by-frame
+path = "test/5.jpg"
+frame = cv2.imread(path)
+
+# resizing for faster detection
+#frame = cv2.resize(frame, (640, 480))
+# using a greyscale picture, also for faster detection
+gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+# detect people in the image
+# returns the bounding boxes for the detected objects
+boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
+
+if len(weights) > 0:
+    print("terdeteksi")
+else:
+    print("!!! tidak terdeteksi !!!")
+
+boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+for (xA, yA, xB, yB) in boxes:
+    # display the detected boxes in the colour picture
+    cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+cv2.imwrite('hog.jpg', frame)
